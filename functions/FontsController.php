0e83aade68108ea937e5ba199369fdd9f20c7284
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-09-21 00:04:19 +0600

    CRUD: fonts logic

--- functions/FontsController.php
+++ functions/FontsController.php
@@ -2,9 +2,70 @@
 
 class FontsController{
 
-    private $fontDir = 'fonts/';
+    private $fontDir;
+    private $db; 
 
-    
+    public function __construct($db)
+    {
+        $this->db = $db;
+        $this->fontDir = 'fonts/';
+    }
+
+    public function uploadFont($fontFile){
+        if($fontFile['type'] != 'font/ttf'){
+            return 
+                [   
+                    'status' => 'false', 
+                    'message' => 'Only TTF files are allowed.'
+                ];
+        }
+
+        $fontName = basename($fontFile['name']);
+        $targetPath = $this->fontDir . $fontName;
+
+        if(move_uploaded_file($fontFile['tmp_name'], $targetPath)){
+            $fontPath = $targetPath; 
+            $this->addFontToDB($fontName, $fontPath);
+
+            return $this->displayFonts();
+        }
+    }
+
+    private function addFontToDB($fontName, $fontPath){
+        $sql = "INSERT INTO fonts (font_name, font_path, status) VALUES (:font_name, :font_path, 1)";
+        $this->db->query($sql, [
+            'font_name' => $fontName, 
+            'font_path' => $fontPath
+        ]);
+    }
+
+    public function displayFonts(){
+        $sql = "SELECT * FROM fonts WHERE status = 1";
+
+        $result = $this->db->query($sql);
+        $fonts = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        $html = '';
+
+        foreach($fonts as $font){
+            $html .= '<li class="list-group-item d-flex justify-content-between align-items-center">';
+            $html .= '<span style="font-family:' . $font['font_name'] . ';" class="font-preview">Example Style (' . $font['font_name'] . ')</span>';
+            $html .= '<span>Path: ' . $font['path'] . '</span>';
+            $html .= '<button class="btn btn-danger btn-sm delete-font" data-id="' . $font['id'] . '">Delete</button>';
+            $html .= '</li>';
+        }
+
+        return $html;
+    }
+
+    public function deleteFont($fontId){
+        $sql = "UPDATE fonts SET status = 0 WHERE id = :id";
+        $this->db->query($sql, [
+            'id' => $fontId
+        ]);
+
+        return $this->displayFonts();
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
     require_once 'includes/header.php';
+    require_once 'database/Database.php'; 
+    require_once 'functions/FontsController.php';
+
+    $db = new Database();
+    $fontsController = new FontsController($db);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['font'])) {
+        $result = $fontsController->uploadFont($_FILES['font']);
+    }
+
+    $fontList = $fontsController->displayFonts();
 ?>
 
 <body>
@@ -21,71 +32,8 @@
         require_once 'includes/scripts.php';
     ?>
 
-    <script>
-        $(document).ready(function () {
-            $('input[type="file"]').on('click', function () {
-                $(".file_names").html("");
-            })
-            if ($('input[type="file"]')[0]) {
-                var fileInput = document.querySelector('label[for="fontFile"]');
-                fileInput.ondragover = function () {
-                    this.className = "et_pb_contact_form_label changed";
-                    return false;
-                }
-                fileInput.ondragleave = function () {
-                    this.className = "et_pb_contact_form_label";
-                    return false;
-                }
-                fileInput.ondrop = function (e) {
-                    e.preventDefault();
-                    var fileNames = e.dataTransfer.files;
-                    for (var x = 0; x < fileNames.length; x++) {
-                        console.log(fileNames[x].name);
-                        $ = jQuery.noConflict();
-                        $('label[for="fontFile"]').append(
-                            "<div class='file_names'>" + fileNames[x].name + "</div>");
-                    }
-                }
-                $('#fontFile').change(function () {
-                    var fileNames = $('#fontFile')[0].files[0].name;
-                    $('label[for="fontFile"]').append(
-                        "<div class='file_names'>" + fileNames + "</div>");
-                    $('label[for="fontFile"]').css('background-color',
-                        '##eee9ff');
-                });
-            }
-        });
-    </script>
-
-    <script>
-        // Upload font via AJAX
-        function uploadFont() {
-            var formData = new FormData(document.getElementById('uploadForm'));
-            fetch('path_to_your_php_file.php', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.text())
-                .then(data => {
-                    document.getElementById('fontList').innerHTML = data;
-                });
-        }
-
-        // Delete font via AJAX
-        function deleteFont(fontId) {
-            fetch('path_to_your_php_file.php', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: `deleteFont=true&fontId=${fontId}`
-                })
-                .then(response => response.text())
-                .then(data => {
-                    document.getElementById('fontList').innerHTML = data;
-                });
-        }
-    </script>
+    <script type="text/javascript" src="assets/js/fontsUploader.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 
 </html>
