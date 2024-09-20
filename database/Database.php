72f83a0700c28e06d81cb625edd7896eab5cc647
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-09-20 15:28:28 +0600

    Create: database connection and directory

---
+++ database/Database.php
@@ -0,0 +1,39 @@
+<?php 
+    class Database
+    {
+        private $host = 'localhost';    
+        private $dbName = 'font-group';
+        private $username = 'root';
+        private $password = '';
+
+        private $con; 
+
+        public function __construct()
+        {
+            $this->connect();
+        }
+
+        // Database Connection 
+        private function connect()
+        {
+            try{
+                $this->con = new PDO("mysql:host={$this->host};dbname={$this->dbName}", $this->username, $this->password);
+                $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(Exception $e){
+                echo "Database connection failed: " . $e->getMessage();
+                die();
+            }
+        }
+        public function getConnection() {
+            return $this->con;
+        }
+
+        // Dynamic optional parameter query
+        public function query($sql, $params = []) {
+            $stmt = $this->con->prepare($sql);
+            $stmt->execute($params);
+            return $stmt;
+        }
+    }
+
+?>
---
+++ functions/FontsController.php
@@ -0,0 +1,10 @@
+<?php 
+
+class FontsController{
+
+    private $fontDir = 'fonts/';
+
+    
+}
+
+?>
